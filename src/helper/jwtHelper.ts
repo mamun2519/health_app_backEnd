5dd271c37c89beffc62303ec0e07bbf0a1ca2dbb
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-10-12 00:09:28 +0600

    add to pagination helper

---
+++ src/helper/jwtHelper.ts
@@ -0,0 +1,18 @@
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken'
+const tokenGenerate = (
+  data: Record<string, unknown>,
+  secret: Secret,
+  expire_in: string,
+): string => {
+  const token = jwt.sign(data, secret, { expiresIn: expire_in })
+  return token
+}
+
+const verifyToken = (token: string, secret: Secret): JwtPayload => {
+  return jwt.verify(token, secret) as JwtPayload
+}
+
+export const jwtHelper = {
+  tokenGenerate,
+  verifyToken,
+}
