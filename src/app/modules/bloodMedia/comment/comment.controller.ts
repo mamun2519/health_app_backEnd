56aac42d9f5f38b52dbddb58c912a7de520aab98
Juboraj Islam Mamun <juborajvai22@gmail.com>
2024-02-02 22:06:10 +0600

    complete updateComment or deleteComment

--- src/app/modules/bloodMedia/comment/comment.controller.ts
+++ src/app/modules/bloodMedia/comment/comment.controller.ts
@@ -29,42 +29,41 @@ const myAllComment = catchAsync(async (req: Request, res: Response) => {
     data: result,
   })
 })
-const myAllComment = catchAsync(async (req: Request, res: Response) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const user = (req as any).user
-  const result = await CommentService.myAllCommentFromDB(user.user_id)
+const getCommentById = catchAsync(async (req: Request, res: Response) => {
+  const result = await CommentService.getCommentByIdFromDB(req.params.id)
   sendApiResponse(res, {
     success: true,
     statusCode: StatusCodes.OK,
-    message: 'My All Comment Retrieve successfully',
+    message: 'Single Comment Retrieve successfully',
+    data: result,
+  })
+})
+const updateCommentById = catchAsync(async (req: Request, res: Response) => {
+  const result = await CommentService.updateCommentIntoDB(
+    req.params.id,
+    req.body,
+  )
+  sendApiResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: ' Comment Update successfully',
     data: result,
   })
 })
 
-const myAllComment = catchAsync(async (req: Request, res: Response) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const user = (req as any).user
-  const result = await CommentService.myAllCommentFromDB(user.user_id)
+const deleteCommentById = catchAsync(async (req: Request, res: Response) => {
+  const result = await CommentService.deleteCommentByIdFromDB(req.params.id)
   sendApiResponse(res, {
     success: true,
     statusCode: StatusCodes.OK,
-    message: 'My All Comment Retrieve successfully',
+    message: 'Comment Delete successfully',
     data: result,
   })
 })
-const myAllComment = catchAsync(async (req: Request, res: Response) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const user = (req as any).user
-  const result = await CommentService.myAllCommentFromDB(user.user_id)
-  sendApiResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'My All Comment Retrieve successfully',
-    data: result,
-  })
-})
-
 export const CommentController = {
   insertComment,
   myAllComment,
+  getCommentById,
+  updateCommentById,
+  deleteCommentById,
 }
