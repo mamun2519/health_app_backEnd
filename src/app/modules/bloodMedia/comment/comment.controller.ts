724ebbd38cafeacb61d9be4f2167245094851192
Juboraj Islam Mamun <juborajvai22@gmail.com>
2024-02-02 22:22:39 +0600

    complete replyCommnet,get,update,delete,create api

--- src/app/modules/bloodMedia/comment/comment.controller.ts
+++ src/app/modules/bloodMedia/comment/comment.controller.ts
@@ -9,7 +9,7 @@ const insertComment = catchAsync(async (req: Request, res: Response) => {
   const user = (req as any).user
   const result = await CommentService.insertCommentIntoDB(
     user.user_id,
-    req.body.post,
+    req.body.comment,
   )
   sendApiResponse(res, {
     success: true,
@@ -60,10 +60,78 @@ const deleteCommentById = catchAsync(async (req: Request, res: Response) => {
     data: result,
   })
 })
+// ReplyCommentController
+const insertReplyComment = catchAsync(async (req: Request, res: Response) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const user = (req as any).user
+  const result = await CommentService.insertReplyCommentIntoDB(
+    user.user_id,
+    req.body.comment,
+  )
+  sendApiResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'Reply Comment successfully',
+    data: result,
+  })
+})
+const myAllReplyComment = catchAsync(async (req: Request, res: Response) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const user = (req as any).user
+  const result = await CommentService.myAllReplyCommentFromDB(user.user_id)
+  sendApiResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'My All Reply Comment Retrieve successfully',
+    data: result,
+  })
+})
+const getReplyCommentById = catchAsync(async (req: Request, res: Response) => {
+  const result = await CommentService.getReplyCommentByIdFromDB(req.params.id)
+  sendApiResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'Reply Comment Retrieve successfully',
+    data: result,
+  })
+})
+const updateReplyCommentById = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await CommentService.updateReplyCommentIntoDB(
+      req.params.id,
+      req.body,
+    )
+    sendApiResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Reply Comment Update successfully',
+      data: result,
+    })
+  },
+)
+
+const deleteReplyCommentById = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await CommentService.deleteReplyCommentByIdFromDB(
+      req.params.id,
+    )
+    sendApiResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Reply Comment Delete successfully',
+      data: result,
+    })
+  },
+)
 export const CommentController = {
   insertComment,
   myAllComment,
   getCommentById,
   updateCommentById,
   deleteCommentById,
+  insertReplyComment,
+  myAllReplyComment,
+  getReplyCommentById,
+  updateReplyCommentById,
+  deleteReplyCommentById,
 }
