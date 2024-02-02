c816a89c7b2f8dd7bf87e94433f71976816cdf81
Juboraj Islam Mamun <juborajvai22@gmail.com>
2024-02-02 22:26:40 +0600

    complete all reply comment api

--- src/app/modules/bloodMedia/comment/comment.route.ts
+++ src/app/modules/bloodMedia/comment/comment.route.ts
@@ -7,15 +7,23 @@ const router = express.Router()
 router.get('/details/:id', CommentController.getCommentById)
 router.patch('/update/:id', CommentController.updateCommentById)
 router.delete('/delete/:id', CommentController.deleteCommentById)
+router.get('/reply/details/:id', CommentController.getReplyCommentById)
+router.patch('reply/update/:id', CommentController.updateReplyCommentById)
+router.delete('reply/delete/:id', CommentController.deleteReplyCommentById)
 router.post(
   '/create',
   auth(USER_ROLE.ADMIN, USER_ROLE.BLOODDONOR, USER_ROLE.USER, USER_ROLE.DOCTOR),
   CommentController.insertComment,
 )
-router.get(
-  '/my-comment',
+router.post(
+  'reply/create',
   auth(USER_ROLE.ADMIN, USER_ROLE.BLOODDONOR, USER_ROLE.USER, USER_ROLE.DOCTOR),
-  CommentController.myAllComment,
+  CommentController.insertReplyComment,
+)
+router.get(
+  '/reply/my-comment',
+  auth(USER_ROLE.ADMIN, USER_ROLE.BLOODDONOR, USER_ROLE.USER, USER_ROLE.DOCTOR),
+  CommentController.myAllReplyComment,
 )
 
 export const CommentRoute = router
