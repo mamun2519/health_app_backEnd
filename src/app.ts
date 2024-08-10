b1adb5795cd69143ded77a288140025a48a3b4d1
mamun2519 <juborajislam46@gmail.com>
2024-08-10 22:53:53 +0600

    change emmail

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use('/api/v1', RootRoutes)
 app.get('/', (req: Request, res: Response) => {
   res
     .status(200)
-    .json({ success: true, message: 'server is run successfully.......' })
+    .json({ success: true, message: 'server is run successfully........' })
 })
 // Global Error Handler
 app.use(globalErrorHandler)
